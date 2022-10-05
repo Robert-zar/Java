0edc18ca23d318f1dfcf5bdc53023ef9ecc59cb7
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2022-10-05 19:59:13 +0300

    Add files via upload

---
+++ lab 2/hashCode/Main.java
@@ -0,0 +1,27 @@
+package JavaNew;
+
+public class Main {
+
+    static class User {
+        private String name;
+        private String surname;
+        private int birthYear;
+
+        User(String name, String surname, int birthYear) {
+            this.name = name;
+            this.surname = surname;
+            this.birthYear = birthYear;
+        }
+
+        @Override
+        public String toString() {
+            return this.name+","+ " " +this.birthYear+"г.";
+        }
+    }
+
+    public static void main(String[] args) {
+        User usl = new User("Иван", "Петров", 1996);
+        System.out.println(usl.toString());
+        System.out.println("Hash:" + usl.hashCode());
+    }
+}
